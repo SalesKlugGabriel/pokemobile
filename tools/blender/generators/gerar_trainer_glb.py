dfc2ec3b0f91e4e1313fac315483009a52c0db1b
Gabriel <saleskluggabriel@gmail.com>
2026-09-15 10:52:09 -0300

    V3: refinar treinador e adicionar vento na grama

--- tools/blender/generators/gerar_trainer_glb.py
+++ tools/blender/generators/gerar_trainer_glb.py
@@ -10,6 +10,11 @@ def material(name, color):
 
 BLUE = material("trainer_blue", (0.04, 0.22, 0.55))
 RED = material("trainer_red", (0.78, 0.06, 0.04))
+SKIN = material("trainer_skin", (0.86, 0.53, 0.34))
+DARK = material("trainer_pants", (0.035, 0.055, 0.085))
+WHITE = material("trainer_shirt", (0.84, 0.86, 0.80))
+HAIR = material("trainer_hair", (0.08, 0.035, 0.018))
+EYE = material("trainer_eye", (0.02, 0.03, 0.04))
 parts = []
 
 def sphere(name, loc, scale, mat=BLUE):
@@ -26,16 +31,21 @@ def cyl(name, loc, radius, depth, mat=BLUE, rot=(0,0,0)):
     o=bpy.context.object; o.name=name; o.data.materials.append(mat); parts.append(o)
 
 # Godot-style construction coordinates: +Y up, -Z is the player's front.
-sphere("head", (0,1.50,-0.04), (0.22,0.24,0.20))
+sphere("head", (0,1.50,-0.04), (0.22,0.24,0.20), SKIN)
+sphere("hair_back", (0,1.57,0.10), (0.21,0.18,0.14), HAIR,)
 cyl("cap", (0,1.72,-0.04), 0.25, 0.10, RED)
 cube("cap_brim", (0,1.69,-0.22), (0.18,0.035,0.13), RED, 0.02)
 cube("torso", (0,1.02,0.0), (0.27,0.38,0.16), BLUE, 0.08)
+cube("shirt_panel", (0,1.08,-0.17), (0.12,0.25,0.025), WHITE, 0.025)
 cube("backpack", (0,1.08,0.22), (0.22,0.28,0.10), RED, 0.05)
+for side in (-1,1):
+    sphere("ear", (.22*side,1.50,-.03), (.035,.065,.035), SKIN)
+    sphere("eye", (.082*side,1.54,-.225), (.026,.038,.018), EYE)
 for s in (-1,1):
     sphere("arm", (0.34*s,1.06,-0.02), (0.09,0.30,0.09))
-    sphere("hand", (0.34*s,0.76,-0.04), (0.10,0.10,0.10), RED)
-    cube("leg", (0.13*s,0.43,0.0), (0.11,0.32,0.12), BLUE, 0.04)
-    cube("boot", (0.13*s,0.10,-0.07), (0.14,0.10,0.20), RED, 0.04)
+    sphere("hand", (0.34*s,0.76,-0.04), (0.10,0.10,0.10), SKIN)
+    cube("leg", (0.13*s,0.43,0.0), (0.11,0.32,0.12), DARK, 0.04)
+    cube("boot", (0.13*s,0.10,-0.07), (0.14,0.10,0.20), DARK, 0.04)
 
 bpy.ops.object.select_all(action='DESELECT')
 for o in parts: o.select_set(True)
@@ -95,10 +105,13 @@ zero={}
 action("PKM_TRAINER_IDLE", [1,20,40], [zero, {"chest":(math.radians(2),0,0)}, zero])
 action("PKM_TRAINER_WALK", [1,10,20], [{"leg_l":(.45,0,0),"leg_r":(-.45,0,0),"arm_l":(-.35,0,0),"arm_r":(.35,0,0)}, zero, {"leg_l":(-.45,0,0),"leg_r":(.45,0,0),"arm_l":(.35,0,0),"arm_r":(-.35,0,0)}])
 action("PKM_TRAINER_RUN", [1,8,16], [{"leg_l":(.75,0,0),"leg_r":(-.75,0,0),"arm_l":(-.55,0,0),"arm_r":(.55,0,0)}, zero, {"leg_l":(-.75,0,0),"leg_r":(.75,0,0),"arm_l":(.55,0,0),"arm_r":(-.55,0,0)}])
+action("PKM_TRAINER_TURN_LEFT", [1,8,16], [zero, {"pelvis":(0,0,math.radians(16)),"chest":(0,0,math.radians(-10))}, zero])
+action("PKM_TRAINER_TURN_RIGHT", [1,8,16], [zero, {"pelvis":(0,0,math.radians(-16)),"chest":(0,0,math.radians(10))}, zero])
+action("PKM_TRAINER_ATTACK_01", [1,7,16], [zero, {"chest":(math.radians(-12),0,0),"arm_r":(math.radians(-75),0,0)}, zero])
 action("PKM_TRAINER_HIT", [1,5,12], [zero, {"chest":(0,0,math.radians(-18)),"head":(0,0,math.radians(-10))}, zero])
 action("PKM_TRAINER_FAINT", [1,12,30], [zero, {"root":(0,0,math.radians(-35))}, {"root":(0,0,math.radians(-70))}])
 
 bpy.ops.object.select_all(action='DESELECT'); arm.select_set(True); mesh.select_set(True); bpy.context.view_layer.objects.active=arm
 os.makedirs(os.path.dirname(OUT), exist_ok=True)
-bpy.ops.export_scene.gltf(filepath=OUT, export_format='GLB', use_selection=True, export_apply=True)
+bpy.ops.export_scene.gltf(filepath=OUT, export_format='GLB', use_selection=True, export_apply=True, export_animations=True, export_nla_strips=True)
 print("WROTE", OUT)
